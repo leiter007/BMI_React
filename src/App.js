f716153c93a96e3b2d65db9396e31d76b9f8fb3d
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-13 12:06:28 +0200

    adds state declaration when component is initialized, and updates state when value of the input for height and weight is changed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div>
           <label>Weight(kg)</label>
-          <input name="weight"></input>
+          <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value })}></input>
         </div>
 
         <div>
           <label>Height(cm)</label>
-          <input name="height"></input>
+          <input name="height" value={this.state.weight} onChange={ (e) => this.setState({ height: e.target.value})}></input>
         </div>
       </div>
     );
